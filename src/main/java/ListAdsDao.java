21064d8428c8aaedc830ef9ffcdd42e35456d7ad
Donald Spini <donald.spini68@gmail.com>
2020-09-18 16:53:15 -0500

    adlister

--- src/main/java/ListAdsDao.java
+++ src/main/java/ListAdsDao.java
@@ -1,9 +1,4 @@
-package com.codeup.adlister.dao;
-
-import com.codeup.adlister.models.Ad;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class ListAdsDao implements Ads {
     private List<Ad> ads;
--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,9 +1,3 @@
-package com.codeup.adlister.controllers;
-
-import com.codeup.adlister.dao.DaoFactory;
-import com.codeup.adlister.models.User;
-import com.codeup.adlister.util.Password;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;

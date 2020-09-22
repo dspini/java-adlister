d0b40f4b969695dbf0870528a3e3d02478f0990e
Donald Spini <donald.spini68@gmail.com>
2020-09-22 09:14:43 -0500

    adlister

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,7 +1,3 @@
-import com.codeup.adlister.dao.DaoFactory;
-import com.codeup.adlister.models.User;
-import com.codeup.adlister.util.Password;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;

9f8c25134c399a183e9583a59e2ded34409fbf92
Donald Spini <donald.spini68@gmail.com>
2020-09-22 15:43:31 -0500

    adlister

--- src/main/java/dao/ListAdsDao.java
+++ src/main/java/dao/ListAdsDao.java
@@ -18,12 +18,9 @@ public class ListAdsDao implements Ads {
     @Override
 
     public Long insert(Ad ad) {
-        // make sure we have ads
         if (ads == null) {
             ads = generateAds();
         }
-        // we'll assign an "id" here based on the size of the ads list
-        // really the database would handle this
         ad.setId((long) ads.size());
         ads.add(ad);
         return ad.getId();
